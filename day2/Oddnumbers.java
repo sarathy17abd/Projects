37ecee107c83bca6d9a296731a006c915b7357ce
sarathy17 <145759979+sarathy17abd@users.noreply.github.com>
2025-10-31 20:59:20 +0530

    Add files via upload
    Week 1

---
+++ day2/Oddnumbers.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Oddnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-geneated method stub
+		for (int i = 1; i <=12; i++) {
+			if (i%2==1) {
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
