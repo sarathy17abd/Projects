967fdce05bc9ccd4e5d2cf2ea7a2d413f29012ec
sarathy17 <145759979+sarathy17abd@users.noreply.github.com>
2025-10-31 21:18:11 +0530

    Update EmployeeDetails.java

--- day1/EmployeeDetails.java
+++ day1/EmployeeDetails.java
@@ -1,25 +1,2 @@
-package week1.day1;
 
-public class EmployeeDetails {
-	
 
-	public static void main(String[] args) {
-		String name="John doe";
-		byte age=30;
-		float salary=55000.75f;
-		char gender='M';
-		boolean acceptTerms=true;
-		long mobileno=9087456679L;
-		int pincode=632503;
-		System.out.println("name="+name);
-		System.out.println("age="+age);
-		System.out.println("salary="+salary);
-		System.out.println("gender="+gender);
-		System.out.println("acceptTerms="+acceptTerms);
-		System.out.println("mobileno="+mobileno);
-		System.out.println("pincode="+pincode);
-				
-
-	}
-
-}
